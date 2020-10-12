756dcc8abeb3d488dc2651cc51673d53b10ebbc3
David Harper <davidgamer789@gmail.com>
2020-10-12 10:20:29 -0500

    Updated with files sent from group members

---
+++ bankAccount.h
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+using namespace std;
+//parent class
+// bank acount 
+class BankAcount
+{
+    protected:
+    string accNum;
+    double accBalance;
+    double annualIR;
+    double annualSC;
+
+    public:
+    // Default
+    BankAcount()
+    {
+        accNum = "";
+        accBalance = 0.0;
+        annualIR = 0.0;
+    }
+    // Constructor
+    BankAcount(string num, double balance, double intrestRate)
+    {
+        accNum  = num;
+        accBalance = balance;
+        annualIR = intrestRate;
+    }
+    // Deposits an amount specified into the account balance
+    virtual void deposit(double ammount)
+    {
+        accBalance += ammount;
+    }
+    // Withdraws an amount specified from the account balance
+    virtual void withdraw(double ammount)
+    {
+        accBalance -= ammount;
+    }
+    // calculates daily interest rate and adds the daily interest to account balance
+    void calcInt()
+    {
+        double dailyIR = annualIR / 365;
+        dailyIR = dailyIR * accBalance;
+        accBalance += dailyIR;
+    }
+
+    // unfinished
+    void yearlyCharge()
+    {
+        accBalance -= annualSC;
+    }
+    //unfinished
+    virtual void closeAcc()
+    {
+
+    }
+
+}
